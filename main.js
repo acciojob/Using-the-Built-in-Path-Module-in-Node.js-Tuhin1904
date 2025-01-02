10a7c2742369829503af5d1c10d5f9623a598606
Tuhin1904 <iamtuhinn@gmail.com>
2025-01-02 18:46:10 +0530

    solved

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const path = require('path');
+const { argv } = require('process');
 
-// TODO: Use path.join to join two file path segments
+function joinPaths(segment1, segment2){
+    return path.join(segment1, segment2);
+}
 
-// TODO: Print the resulting path to the console
+const args = argv.slice(2);
+
+if(args.length < 2){
+    console.error(`Please provide two file path segments as arguments`);
+    process.exit(1);
+}
+
+const [segment1, segment2] = args;
+
+const joinedPath = joinPaths(segment1, segment2);
+console.log(`Joined Path: ${joinedPath}`)
+
+// DO NOT EDIT BELOW THIS LINE
+module.exports = { joinPaths };
